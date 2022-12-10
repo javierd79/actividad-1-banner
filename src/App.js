f31a0f9841a144ebcf52152855962c178e661742
Javier <javierdiazt406@gmail.com>
2022-12-10 18:08:40 -0400

    banner

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ function App() {
          <span className="navbar-brand mb-0 h1">Texto</span>
          <h1>ola</h1>
       </Navbar>
-      <Navbar>
-        <span className="navbar-brand mb-0 h1">prueba</span>
-        <h2>ola</h2>
-      </Navbar>
     </div>
   );
 }
